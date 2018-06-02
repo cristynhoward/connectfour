4ca02416445496d25b9bccc1906e16d218e2bd8a
Cristyn Howard <cristyn.howard@mail.utoronto.ca>
2018-06-02 05:16:18 -0400

    Added phase 3.

---
+++ phase3/gamehelpers.py
@@ -0,0 +1,100 @@
+from time import sleep
+from phase1.helpers import *
+
+
+def limit_handled(cursor):
+    """ Iterates with a cursor object, pauses for 15 min when given a RateLimitException.
+    Code taken from: http://docs.tweepy.org/en/v3.5.0/code_snippet.html#pagination
+
+    :param cursor: The cursor to be rate limited.
+    :type cursor: Cursor
+    """
+    while True:
+        try:
+            yield cursor.next()
+        except tweepy.RateLimitError:
+            sleep(15 * 60)
+
+
+def get_next_game_id():
+    """Get the ID for the next new game.
+
+    :return: the ID for the next new game.
+    :rtype: int
+    """
+    with open(os.path.join(getpath(), ".data/next_game_id.txt"), 'rw+') as f:
+        out = int(f.read())
+        f.seek(0)
+        f.write(str(out + 1))
+        f.truncate()
+    return out
+
+
+def load_next_game_reply():
+    """ Return next game to be tweeted out.
+    :return: next game to be tweeted out.
+    :rtype: str
+    """
+    with open(os.path.join(getpath(), '.data/queue.csv'), 'a+') as f:
+        last_wrote = get_last_wrote()
+        for line in f:
+            tokens = line.split(",")
+            if tokens[3] > last_wrote:
+                return str(line)
+        return None
+
+
+def record_game(gamestring):
+    """ Store gamestring as entry in gamelist.csv.
+
+    :param gamestring: a string representation of the game to be recorded.
+    :type gamestring: str
+    """
+    with open(os.path.join(getpath(), '.data/gamelist.csv'), 'a+') as f:
+        f.write(gamestring + "\n")
+    log("Recorded " + gamestring)
+
+
+def retrieve_game(last_tweet_id):
+    """ Retrieve from the gamelist the game whose last tweet has last_tweet_id.
+
+    :param last_tweet_id: the ID of the last tweet in the game to be retrieved.
+    :type last_tweet_id: str
+    :return: gamestring of game iff game with last_tweet_id exists, else None.
+    :rtype: str, None
+    """
+    with open(os.path.join(getpath(), '.data/gamelist.csv'), 'a+') as f:
+        for line in f:
+            tokens = line.split(",")
+            if int(tokens[3]) == int(last_tweet_id):
+                return str(line)
+        return None
+
+
+def record_outgoing_game(gamestring):
+    """ Store gamestring as entry in queue.csv, the list for tweets waiting to be sent.
+
+    :param gamestring: a string representation of the game to be recorded.
+    :type gamestring: str
+    """
+    with open(os.path.join(getpath(), '.data/queue.csv'), 'a+') as f:
+        f.write(gamestring + "\n")
+    log("Recorded outgoing " + gamestring)
+
+
+def retrieve_outgoing_game(last_tweet_id):
+    """ Retrieve from the queue of games waiting to be tweeted the game whose last
+    tweet has last_tweet_id.
+
+    :param last_tweet_id: the ID of the last tweet in the game to be retrieved.
+    :type last_tweet_id: str
+    :return: gamestring of game iff game with last_tweet_id exists, else None.
+    :rtype: str, None
+    """
+    with open(os.path.join(getpath(), '.data/queue.csv'), 'a+') as f:
+        for line in f:
+            print(line)
+            tokens = line.split(",")
+            if tokens[3] == last_tweet_id:
+                return str(line)
+        return None
---
+++ phase3/tweet_processor.py
@@ -0,0 +1,77 @@
+from phase2.ConnectFourGame import *
+from phase3.gamehelpers import *
+import emoji
+
+
+def process_mentions():
+    """
+
+    :return:
+    :rtype:
+    """
+    api = get_twitter_api()
+    first = True
+    since_id = get_read_since()
+    newest_tweet_id = None
+
+    for tweet in limit_handled(tweepy.Cursor(api.mentions_timeline).items()):
+        if int(tweet.id_str) <= int(since_id):
+            return
+        if first is True:
+            newest_tweet_id = tweet.id_str
+            first = False
+
+        #  NEW GAME
+        if len(tweet.entities[u'user_mentions']) > 1:  # at least one other user mentioned
+            if tweet.in_reply_to_status_id is None:  # not reply to another tweet
+                if len(tweet.text.split(" ")) > 1:  # has second command
+                    if tweet.text.split(" ")[1] == "new":  # second command is new
+                        user1 = tweet.user.screen_name
+                        user2 = tweet.entities[u'user_mentions'][1][u'screen_name']
+                        newgame = ConnectFourGame.new_game(get_next_game_id(), user1, user2, tweet.id_str)
+                        record_game(newgame.game_to_string())
+                        record_outgoing_game(newgame.game_to_string())
+                        log("Created new game: " + newgame.game_to_string())
+        else:
+            #  PLAY TURN
+            gamestring = retrieve_game(tweet.in_reply_to_status_id)
+            if gamestring is not None:
+                game = ConnectFourGame.game_from_string(gamestring)
+                if game.game_won == 0:
+                    active_user = game.user2
+                    if game.a_is_playing == 1:
+                        active_user = game.user1
+                    if tweet.user.screen_name == active_user:
+                        token = tweet.text.split(" ")[2]
+                        if any(token == s for s in ["1", "2", "3", "4", "5", "6", "7"]):
+                            game.play_turn(int(tweet.id_str), int(token))
+                            record_game(game.game_to_string())
+                            record_outgoing_game(game.game_to_string())
+                            log(active_user + " played a " + token +
+                                " resulting in game: " + game.game_to_string())
+
+    log("Processed mentions from " + str(since_id) + " to " + str(newest_tweet_id) + ".")
+    set_read_since(newest_tweet_id)
+
+
+def tweet():
+    """
+    :return:
+    :rtype:
+    """
+    api = get_twitter_api()
+    gamestring = load_next_game_reply()
+    if gamestring is None:
+        log("No game replies to be made.")
+    else:
+        try:
+            game = ConnectFourGame.game_from_string(gamestring)
+            sent = api.update_status(emoji.emojize(game.asemoji()), game.last_tweet)
+        except tweepy.error.TweepError as e:
+            log(e.message)
+        else:
+            set_last_wrote(game.last_tweet)
+            game.last_tweet = sent.id_str
+            record_game(game.game_to_string())
+            log("Tweeted "+game.game_to_string())
+            tweet()
