9c096ce81e7d7611c73424974648dcb979eb5559
Cristyn Howard <cristyn.howard@mail.utoronto.ca>
2018-06-19 23:50:03 -0400

    Added database cleanup script.

---
+++ database_cleanup.py
@@ -0,0 +1,33 @@
+""" Removes won and inactive games from database.
+"""
+from ConnectFourGame import ConnectFourGame
+from databasehelpers import *
+from datetime import *
+
+
+def cleanup_active_games():
+    """ Scan all documents in Active Games collection.
+    Remove any that are completed or expired.
+
+    :return: None
+    :rtype: None
+    """
+    db = access_database()
+    if db is not None:
+        active_games = db.Active_Games
+
+        for document in active_games.find():
+            game_to_check = ConnectFourGame.game_from_string(document["game"])
+
+            diff = datetime.now() - game_to_check.last_active
+            if diff.days > 14:
+                remove_active_game(game_to_check.last_tweet)
+                log("Removed expired game: " + game_to_check.game_to_string())
+
+            if game_to_check.game_won == 1:
+                remove_active_game(game_to_check.last_tweet)
+                log("Removed completed game: " + game_to_check.game_to_string())
+
+
+if __name__ == '__main__':
+    cleanup_active_games()
